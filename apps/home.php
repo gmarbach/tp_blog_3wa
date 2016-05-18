79079a60d9516ac165691e0086179c547bad0f52
patson67 <patson@wanadoo.fr>
2016-05-18 13:11:33 +0200

    maj home tri

--- apps/home.php
+++ apps/home.php
@@ -1,5 +1,25 @@
 <?php
 	$error = '';
 	$noTask = '';
-require('views/home.phtml');
+	$selectSortType = [
+						'createDate' => ['selected','',''],
+						'dueDate' 	 => ['','selected',''],
+						'priority' 	 => ['','','selected']
+	];
+
+	$selectOrderType = [
+						'asc' => ['selected',''],
+						'desc' 	 => ['','selected']
+	];
+
+
+		$sortTypeTest = 'createDate';
+		$sortOrder = 'asc';
+
+	if (isset($_GET['sortType'], $_GET['orderType'])){
+		$sortTypeTest = $_GET['sortType'];
+		$sortOrder = $_GET['orderType'];
+	}
+
+	require('views/home.phtml');
 ?>
