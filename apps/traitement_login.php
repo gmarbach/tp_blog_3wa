58f1570b52c1546ed03767266fecbe5a238d1390
germain009 <reasonres@gmail.com>
2016-05-18 13:36:17 +0200

    nouveau1

--- apps/traitement_login.php
+++ apps/traitement_login.php
@@ -1,39 +1,31 @@
 <?php
 	$error='';
-	if (isset($_POST['login'], $_POST['pass']))
+	if (isset($_POST['login'], $_POST['pwd']))
 	{
-		$login = $_POST['login'];
-		$pass = $_POST['pass'];
-		$users = array();
-		$i = 0;
-		$userlist = fopen('users.json', 'r');
-		if ($userlist !== false)
-		{
-			while (($data = (fgets($userlist))) !== false)
-			{
-				$user = json_decode($data);
-				$users[$i] = $user;
-				$i++;
-			}
-			fclose($userlist);
-			$i = 0;
-			while ($i < count($users))
-			{
-				if ($users[$i][3] == $login)
-				{
-					if ($users[$i][4] == $pass)
-					{
-						$_SESSION['login'] = $login;
-						header('Location: index.php?page=home');
-						exit;
-					}
-					else
-						$error = 'Mot de passe invalide';
-				}
-				$i++;
-			}
-			$error = 'Utilisateur non trouvé';
-		}
-		$error = 'Erreur interne';
+
+
+$bdd = new PDO('mysql:host=localhost;dbname=blog','root','troiswa');
+
+$req = $bdd -> query('SELECT login,pwd FROM register');
+
+while ($donnees = $req ->fetch())
+{
+    if ($_POST['login'] == $donnees['login'] AND 
+    	$_POST['pwd'] == $donnees['pwd'])
+    {
+     echo 'azerty' ;
+
+     header('Location:index.php?page=register');
+
+    }
+}
+
+		
+
+
+
+			
+		
+		
 	}
 ?>
