c347ebd049902c310795dc3fbd2c7dbaab4426ad
patson67 <patson@wanadoo.fr>
2016-05-19 15:31:09 +0200

    maj

--- apps/article.php
+++ apps/article.php
@@ -1,18 +1,23 @@
 <?php
-if (isset($_GET['artId'])
-{
+if (isset($_GET['artId'])){
+
 	$artId=$_GET['artId'];
-	$query='"SELECT * FROM articles WHERE id='.$artId.'"';
-	$res= mysqli_query($link,$query);
+	$query="SELECT * FROM articles WHERE id=".$artId;
+	// var_dump($query);
+	$res= mysqli_query($link,$query) or die("erreur connection");
 	$ligne=mysqli_fetch_assoc($res);
 		
 
-			$artTitle = $ligne['titre'];
-			$artContent = $ligne['contenu'];
-			$artAuthor = $ligne['autheur'];
-			$artCreaDate = $ligne['date_creation'];
-			// $artId = $ligne['id'];
-			require('views/article.phtml');
+	$artTitle = $ligne['titre'];
+	$artContent = $ligne['contenu'];
+	$artAuthor = $ligne['autheur'];
+	$artCreaDate = $ligne['date_creation'];
+	// $artId = $ligne['id'];
+	require('views/article.phtml');
 			
 }	
+else{
+	$error = "erreur interne";
+}
 ?>
+
