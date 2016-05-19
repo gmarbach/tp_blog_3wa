6beba2d604a9bc3dd5d7f08b263c806be16da248
Steph67stbg <melstephpro.stbg67@orange.fr>
2016-05-19 16:27:46 +0200

    new

--- apps/traitement_login.php
+++ apps/traitement_login.php
@@ -3,7 +3,7 @@
 	if (isset($_POST['login'], $_POST['pwd']))
 	{
 
-        $comparer = "SELECT login,pwd FROM register";
+        $comparer = "SELECT login, pwd, profil FROM register";
         $res = mysqli_query($link,$comparer);
 
         while($lignes = mysqli_fetch_assoc($res))
@@ -11,6 +11,11 @@
         	if($lignes['login'] == $_POST['login'] && $lignes['pwd'] == $_POST['pwd'] )
         	{
                 $_SESSION['login'] = $lignes['login'];
+
+                $_SESSION['profil'] = $lignes['profil'];
+
+                var_dump($lignes['profil']);
+
         		header('Location:index.php?page=home');
                 exit;
         	}
