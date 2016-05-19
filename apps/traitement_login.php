80ff7e3a3e865e741fbc3e18f48399c4e916a350
germain009 <reasonres@gmail.com>
2016-05-19 12:12:44 +0200

    re

--- apps/traitement_login.php
+++ apps/traitement_login.php
@@ -4,7 +4,7 @@
 	{
 
 
-    $comparer = "SELECT login,password FROM register";
+    $comparer = "SELECT login,pwd FROM register";
 
     $res = mysqli_query($link,$comparer);
 
@@ -13,7 +13,7 @@
 
 
     	if($lignes['login'] = $_POST['login'] && 
-    		$lignes['password'] = $_POST['password'] )
+    		$lignes['pwd'] = $_POST['pwd'] )
 
     	{
 
