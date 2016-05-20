41adc8f75ca78245c0b7b7b81c7937e093536c56
gmarbach <marbachweb@gmail.com>
2016-05-20 13:30:00 +0200

    traite comment

--- apps/traitement_artComment_crea.php
+++ apps/traitement_artComment_crea.php
@@ -1,16 +1,40 @@
 <?php
+	$error='';
+	
+	if (isset($_POST['title'], $_POST['content']))
+	{
+		$title = $_POST['title'];
+		$content = $_POST['content'];
+		
+		$idArticle= "SELECT id FROM articles";
+		$resOne = mysqli_query($link,$idArticle);
+
+		$idUser="SELECT id FROM register";
+		$resAlpha= mysqli_query($link,$idUser);
 
 
-if(isset($_POST['title'],$_POST['content']))
 
+		
+		if (strlen($title) < 4)
+			$error = 'Titre trop court (4 à 60 caractères)';
+		else if (strlen($title) > 63)
+			$error = 'Titre trop long (4 à 60 caractères)';
+		if (strlen($content) < 3)
+			$error = 'contenu trop court (4 à 250 caractères)';
+		else if (strlen($content) > 2047)/** Pascal : Taille dans la db : 2047 **/
+			$error = 'contenu trop long (4 à 2047 caractères)';
+		
+		if (empty($error))
+		{
 
-{
+			$insert = "INSERT INTO comments (titre, contenu, id_author, id_article) VALUES ('".$title."', '".$content."', '".$resAlpha."', '".$resOne."')";
+			$res = mysqli_query($link, $insert);
 
+			header('Location: index.php?page=index');
+			var_dump($res);
 
-
-HEADER('Location:index.php?page=home');
-}
-
-require('views/artCommentCrea.phtml');
-
+		}
+		
+	}
+?>
 ?>
